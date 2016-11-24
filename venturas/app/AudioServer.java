daf4ffce6284fe7b038079ea18deec9d2dab7a6f
Matthew Chambault <matthewchambault@Matthews-MacBook-Pro.local>
2016-11-24 15:51:00 +0000

    audio app

---
+++ venturas/app/AudioServer.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class AudioServer {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0)
+            throw new IllegalArgumentException("expected sound file arg");
+        File soundFile = AudioUtil.getSoundFile(args[0]);
+
+        System.out.println("server: " + soundFile);
+
+        try (ServerSocket serverSocker = new ServerSocket(6666);
+            FileInputStream in = new FileInputStream(soundFile)) {
+            if (serverSocker.isBound()) {
+                Socket client = serverSocker.accept();
+                OutputStream out = client.getOutputStream();
+
+                byte buffer[] = new byte[2048];
+                int count;
+                while ((count = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+										try {
+										System.out.println("Doing something");
+										Thread.sleep(1000);
+									} catch (InterruptedException e) {
+
+									}
+								}
+            }
+        }
+
+        System.out.println("server: shutdown");
+    }
+}
